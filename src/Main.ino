bc2538002cde6d9f487e6f9ed61828cabb242d5f
Erik Nomitch <enomitch@gmail.com>
2016-07-16 07:51:09 -0500

    basic button/led working

--- src/Main.ino
+++ src/Main.ino
@@ -1,5 +1,13 @@
+#define LED_STATUS 7
+#define SIG_BUTTON 8
+
 void setup() {
+  pinMode(LED_STATUS, OUTPUT);
+  pinMode(SIG_BUTTON, INPUT);
 }
 
 void loop() {
+  digitalWrite(LED_STATUS, digitalRead(SIG_BUTTON));
+
+  delay(100);
 }
