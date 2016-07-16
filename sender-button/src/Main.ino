49ff4c2afdf7a12f502f43e9a13798bc41e234d0
Erik Nomitch <enomitch@gmail.com>
2016-07-16 11:25:34 -0500

    splits sender-button and receiver-signaler

---
+++ sender-button/src/Main.ino
@@ -0,0 +1,54 @@
+#define LED_STATUS 7
+#define SIG_BUTTON 8
+#include "env.h"
+#include "utility.h"
+#include <SPI.h>
+#include <nRF24L01.h>
+#include <RF24.h>
+#include <RF24_config.h>
+
+int msg[1];
+RF24 radio(9,10);
+const uint64_t pipe = 0xE8E8F0F0E1LL;
+
+void ring() {
+  blink(LED_STATUS, 100, 50); 
+}
+
+void setup() {
+  pinMode(LED_STATUS, OUTPUT);
+  pinMode(SIG_BUTTON, INPUT);
+
+  Serial.begin(9600);
+  radio.begin();
+  radio.openWritingPipe(pipe);
+}
+
+// http://shanes.net/another-nrf24l01-sketch-string-sendreceive/
+void loop() {
+  /*if (pinIsHigh(SIG_BUTTON)) {*/
+    /*ring();*/
+  /*}*/
+
+  /*delay(10);*/
+
+    String theMessage = "Hello there!";
+  int messageSize = theMessage.length();
+  for (int i = 0; i < messageSize; i++) {
+    int charToSend[1];
+    charToSend[0] = theMessage.charAt(i);
+    radio.write(charToSend,1);
+  }  
+//send the 'terminate string' value...  
+  msg[0] = 2; 
+  radio.write(msg,1);
+/*delay sending for a short period of time.  radio.powerDown()/radio.powerupp
+//with a delay in between have worked well for this purpose(just using delay seems to
+//interrupt the transmission start). However, this method could still be improved
+as I still get the first character 'cut-off' sometimes. I have a 'checksum' function
+on the receiver to verify the message was successfully sent.
+*/
+  radio.powerDown(); 
+  delay(1000);
+  radio.powerUp();
+}
---
+++ sender-button/src/utility.h
@@ -0,0 +1,75 @@
+// ===============================================
+// UTILITY =======================================
+// ===============================================
+#define arrayLength(x) (sizeof(x) / sizeof(*x))
+
+// -----------------------------------------------
+// PINS ------------------------------------------
+// -----------------------------------------------
+void pinHigh(int pin) {/*{{{*/
+  digitalWrite(pin, HIGH);
+}
+
+void pinLow(int pin) {
+  digitalWrite(pin, LOW);
+}
+
+void pinToggle(int pin) {
+  digitalWrite(pin, !digitalRead(pin));
+}
+
+bool pinSet(int pin, bool state) {
+  if (state) {
+        pinHigh(pin);
+        return true;
+      }
+  pinLow(pin);
+  return false;
+}
+
+bool pinIsHigh(int pin) {
+  return (digitalRead(pin) == HIGH);
+}
+
+bool pinIsLow(int pin) {
+  return (digitalRead(pin) == LOW);
+}
+
+void pinsOutput(int pins[]) {
+  for (int i = 0; i < arrayLength(pins); i++) {
+      pinMode(pins[i], OUTPUT);
+    }
+}/*}}}*/
+
+// -----------------------------------------------
+// BLINKING --------------------------------------
+// -----------------------------------------------
+void blink(int pin, int blinkMs, int blinkTimes) {/*{{{*/
+  for (int i = 0; i < blinkTimes; i++) {
+        pinHigh(pin);
+        delay(blinkMs);
+        pinLow(pin);
+        delay(blinkMs);
+      }
+}
+
+void blinkWithStartState(int pin, int blinkMs, int blinkTimes, bool startState) {
+  for (int i = 0; i < blinkTimes; i++) {
+      pinSet(pin, !startState);
+      delay(blinkMs);
+      pinSet(pin, startState);
+      delay(blinkMs);
+    }
+}/*}}}*/
+
+// -----------------------------------------------
+// SERIAL ----------------------------------------
+// -----------------------------------------------
+/*{{{*/
+// WARNING: I believe this will only work in screen.
+void clearSerial() {
+  Serial.write(27); // Print 'esc'
+  Serial.print("[2J");
+}
+/*}}}*/
+
