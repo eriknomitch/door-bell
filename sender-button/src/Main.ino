f8fec59a0474138ab7ee83dba7fd21bee47d59b3
Erik Nomitch <enomitch@gmail.com>
2016-07-16 19:31:47 -0500

    replaces run script; cleaned

--- sender-button/src/Main.ino
+++ sender-button/src/Main.ino
@@ -7,6 +7,8 @@
 #include <RF24.h>
 #include <RF24_config.h>
 
+#define SERIAL_BAUD 9600
+
 int msg[1];
 RF24 radio(9,10);
 const uint64_t pipe = 0xE8E8F0F0E1LL;
@@ -19,33 +21,31 @@ void setup() {
   pinMode(LED_STATUS, OUTPUT);
   pinMode(SIG_BUTTON, INPUT);
 
-  Serial.begin(9600);
+  Serial.begin(SERIAL_BAUD);
+
   radio.begin();
   radio.openWritingPipe(pipe);
 }
 
 // http://shanes.net/another-nrf24l01-sketch-string-sendreceive/
 void loop() {
-  /*if (pinIsHigh(SIG_BUTTON)) {*/
-    /*ring();*/
-  /*}*/
+ 
+  // Wait until the button is pressed
+  while (pinIsLow(SIG_BUTTON)) { delay(1); }
 
-  /*delay(10);*/
-  
-  while (pinIsLow(SIG_BUTTON)) {
-    delay(1);
-  }
-
+  // It's pressed. Send the message.
   String theMessage = "Hello there!";
   int messageSize = theMessage.length();
   for (int i = 0; i < messageSize; i++) {
-  int charToSend[1];
-  charToSend[0] = theMessage.charAt(i);
-  radio.write(charToSend,1);
-  }  
-//send the 'terminate string' value...  
+    int charToSend[1];
+    charToSend[0] = theMessage.charAt(i);
+    radio.write(charToSend,1);
+  }
+
+  //send the 'terminate string' value...  
   msg[0] = 2; 
   radio.write(msg,1);
+
   /*delay sending for a short period of time.  radio.powerDown()/radio.powerupp
   //with a delay in between have worked well for this purpose(just using delay seems to
   //interrupt the transmission start). However, this method could still be improved
@@ -53,7 +53,6 @@ void loop() {
   on the receiver to verify the message was successfully sent.
   */
   radio.powerDown();
-
   delay(1000);
   radio.powerUp();
 }
